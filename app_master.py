c4147b39fff116ac79956b3788a04654b2f0fe0e
ProfRenatoPereira <105118481+ProfRenatoPereira@users.noreply.github.com>
2026-08-22 22:12:03 -0300

    Update app_master.py

--- app_master.py
+++ app_master.py
@@ -164,5 +164,5 @@ except Exception as e:
     print(f"[TERADMAS ERROR] Falha ao registrar Estrutura: {e}")
 
 if __name__ == '__main__':
-    porta = int(os.environ.get("PORT", 10000))
-    app.run(host='0.0.0.0', port=porta)
+    porta = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=porta, debug=True)
