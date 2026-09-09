2246e5b678aebd736d9a62b99dc2e6177656de35
ProfRenatoPereira <105118481+ProfRenatoPereira@users.noreply.github.com>
2026-09-09 18:47:06 -0300

    Update inicializacao.js

--- configuracao/inicializacao.js
+++ configuracao/inicializacao.js
@@ -6,7 +6,6 @@ function mudarFonte(direcao) {
     document.documentElement.style.fontSize = tamanhoFonteAtual + 'px';
 }
 
-
 function alternarModoEscuro() {
     document.body.classList.remove('alto-contraste');
     document.body.classList.toggle('dark-mode');
